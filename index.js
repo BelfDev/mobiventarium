f1a2ab7496a93c25627ce5eb9f12562e0ebe0546
Pedro Belfort <belfortdev@gmail.com>
2018-09-13 22:50:52 -0300

    Add "axios" dependency

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { Navigation } from "react-native-navigation";
 import WelcomeScreen from "./src/screen/WelcomeScreen";
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => WelcomeScreen);
+Navigation.registerComponent(`com.mobiventarium.WelcomeScreen`, () => WelcomeScreen);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
       component: {
-        name: "navigation.playground.WelcomeScreen"
+        name: "com.mobiventarium.WelcomeScreen"
       }
     }
   });

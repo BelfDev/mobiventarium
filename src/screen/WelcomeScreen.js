608db3dc67b5ee8859e58bfa87927c3c7c4278fc
Leo Moraes <leonardo.moraes@hotmail.com>
2018-09-24 23:31:55 -0300

    adding sign up and sign in on firebase

--- src/screen/WelcomeScreen.js
+++ src/screen/WelcomeScreen.js
@@ -1,41 +1,105 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import firebase from 'react-native-firebase';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
 
-class WelcomeScreen extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      isAuthenticated: false,
-    };
+export default class auth extends Component {
+  state = {
+    email: '',
+    password: '',
+    errorMessage: null,
+    email_login: '',
+    password_login: '',
+    errorMessage_login: null
   }
 
-  componentDidMount() {
-    firebase.auth().signInAnonymouslyAndRetrieveData()
-      .then((credential) => {
-        if (credential) {
-          console.log('default app user ->', credential.user.toJSON())}
-        this.setState({
-          isAuthenticated: true,
-        });
-      });
+  handleSignUp = () => {
+    firebase
+      .auth()
+      .createUserAndRetrieveDataWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => console.log("criado"))
+      .catch(error => this.setState({ errorMessage: error.message }))
+  }
+  handleLogin = () => {
+    const { email_login, password_login } = this.state
+    firebase
+      .auth()
+      .signInAndRetrieveDataWithEmailAndPassword(email_login, password_login)
+      .then((credential) =>{
+        console.log("==========autenticado=======")
+        console.log('user=======>', credential.user.toJSON())
+      })
+      .catch(error => this.setState({ errorMessage: error.errorMessage_login }))
   }
 
   render() {
-    //If the user has not authenticated
-    if (!this.state.isAuthenticated) {
-      return null;
-    }
-
     return (
-      <View>
-        <Text>Welcome to my awesome app!</Text>
-        <Text>autenticado!</Text>
+      <View style={styles.container}>
+        <Text style={{fontSize: 20,color:'red'}}>Sign Up</Text>
+        {this.state.errorMessage &&
+          <Text style={{ color: 'red' }}>
+            {this.state.errorMessage}
+          </Text>}
+        <TextInput
+          placeholder="Email"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+        <Button title="Sign Up" onPress={this.handleSignUp} />
+
+        <View style={styles.separator}></View>
+        
+        <Text style={{fontSize: 20,color:'red'}}>Login</Text>
+        {this.state.errorMessage_login &&
+          <Text style={{ color: 'red' }}>
+            {this.state.errorMessage_login}
+          </Text>}
+        <TextInput
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="Email"
+          onChangeText={email_login => this.setState({ email_login })}
+          value={this.state.email_login}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="Password"
+          onChangeText={password_login => this.setState({ password_login })}
+          value={this.state.password_login}
+        />
+        <Button title="Login" onPress={this.handleLogin} />
       </View>
-    );
+    )
   }
-
 }
-
-export default WelcomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 8
+  },
+  separator:{
+    height:30,
+    width:'100%',
+    backgroundColor:'green',
+    marginVertical:20,
+  },
+})

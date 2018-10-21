6fe581ec3a57669c745588ef6bd48c41461b221c
Pedro Belfort <belfortdev@gmail.com>
2018-10-21 18:55:27 -0300

    Style top bar

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ import Stores from "./src/stores/RootStores";
 import { registerScreens } from "./src/screens";
 import { Navigation } from "react-native-navigation";
 import { Provider } from "mobx-react";
-import Colors from "./src/utils/Colors"
+import Colors from "./src/utils/Colors";
 
 registerScreens(Stores, Provider);
 
 Navigation.events().registerAppLaunchedListener(() => {
-
   Navigation.setDefaultOptions({
     statusBar: {
       visible: true,
       backgroundColor: Colors.topBarPurple,
-      style: 'light'
+      style: "light"
     },
     topBar: {
       visible: true,
       elevation: 4,
       title: {
-        text: 'Inventory Manager',
+        text: "Inventory Manager",
         fontSize: 20,
-        color: 'white',
-        fontWeight: '500',
-        alignment: 'center'
+        color: "white",
+        fontWeight: "500",
+        alignment: "center"
       },
       background: {
         color: Colors.topBarPurple,
+        translucent: true,
+        blur: false
+      },
+      largeTitle: {
+        visible: true,
+        fontSize: 30,
+        color: "white"
       }
     }
   });

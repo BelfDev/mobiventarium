cbe6c7731b231439a7343afcb032b83772ae5552
Leo Moraes <leonardo.moraes@hotmail.com>
2018-10-29 20:15:10 -0300

    Remove inline style

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -49,39 +49,38 @@ export default class WelcomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ height: '50%', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.imageContainer}>
           <Image
-            style={{ width: 100, height: 100 }}
+            style={styles.image}
             source={require('../Images/login1.png')}
             resizeMode="contain"
           />
-          <Text style={{ fontSize: 16, marginTop: 10, color: '#E2E4F6' }}>INVENTORY</Text>
+          <Text style={styles.title}>INVENTORY</Text>
         </View>
-        <View style={{ height: '40%', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={{ fontSize: 30, color: '#FFFFFF', textAlign: 'center', marginTop: 10, }}>Controle de</Text>
-          <Text style={{ fontSize: 30, color: '#FFFFFF', marginTop: 5, textAlign: 'center' }}>Inventário</Text>
-          <Text style={{ fontSize: 15, color: '#E2E4F6', marginTop: 15, textAlign: 'center' }}>Chega de devices</Text>
-          <Text style={{ fontSize: 15, color: '#E2E4F6', marginTop: 5, textAlign: 'center' }}>perdidos</Text>
+        <View style={styles.textContainer}>
+          <Text style={[styles.text, { marginTop: 10 }]}>Controle de</Text>
+          <Text style={[styles.text, { marginTop: 5 }]}>Inventário</Text>
+          <Text style={[styles.secondaryText, { marginTop: 15 }]}>Chega de devices</Text>
+          <Text style={[styles.secondaryText, { marginTop: 5 }]}>perdidos</Text>
         </View>
-        <View style={{ height: '10%', width: '100%', flexDirection: 'row', backgroundColor: 'white' }}>
-      <View style={{width:'50%',alignItems:'center',justifyContent:'center'}}>
-          <Button
-            compact
-            style={styles.signUpButton}
-            onPress={this.pushSignUpScreen}
-          >
-            <Text style={{fontSize:18,color:'#5861C5'}}> Cadastrar </Text>
-          </Button>
+        <View style={styles.buttonContainer}>
+          <View style={styles.signUpContainer}>
+            <Button
+              compact
+              style={styles.signUpButton}
+              onPress={this.pushSignUpScreen}
+            >
+              <Text style={styles.signUpText}> Cadastrar </Text>
+            </Button>
           </View>
-          <View style={{height:'50%',width:1,backgroundColor:'#F2F2F2',alignSelf:'center'}}></View>
-          <View style={{width:'50%',backgroundColor:' green',justifyContent:'center',alignItems:'center'}}>
-          <Button
-            mode="contained"
-            compact
-            style={styles.loginButton}
-            onPress={this.pushLoginScreen}
-          >
-            <Text style={{color:'white',fontSize:18}}> Login </Text>
+          <View style={styles.loginContainer}>
+            <Button
+              mode="contained"
+              compact
+              style={styles.loginButton}
+              onPress={this.pushLoginScreen}
+            >
+              <Text style={styles.loginText}> Login </Text>
             </Button>
           </View>
         </View>
@@ -96,12 +95,59 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#636BD2'
   },
+  imageContainer: {
+    height: '50%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 10,
+    color: '#E2E4F6'
+  },
+  textContainer: {
+    height: '40%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 30,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  secondaryText: {
+    fontSize: 15,
+    color: '#E2E4F6',
+    marginTop: 15,
+    textAlign: 'center'
+  },
   textInput: {
     height: 40,
     width: '90%',
     borderColor: 'gray',
     borderWidth: 1,
     marginTop: 8
+  },
+  buttonContainer:{
+    height: '10%', 
+    width: '100%', 
+    flexDirection: 'row', 
+    backgroundColor: 'white' 
+  },
+  signUpContainer:{
+    width: '50%', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  signUpText:{
+    fontSize: 18, 
+    color: '#5861C5'
   },
   separator: {
     height: 30,
@@ -110,20 +156,29 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
   signUpButton: {
-    flex:1,
-    width:'100%',
+    flex: 1,
+    width: '100%',
     padding: 10,
-    alignItems:'center',
-    justifyContent:'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginContainer:{
+    width: '50%', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  loginText:{
+    color: 'white', 
+    fontSize: 18
   },
   loginButton: {
-    width:'70%',
+    width: '70%',
     backgroundColor: '#5861C5',
     padding: 8,
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:60,
-    marginVertical:5
-    
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 60,
+    marginVertical: 5
+
   },
 })

d49ef32fa8f228cdbb60587f4b51d68a911e6ad0
Pedro Belfort <belfortdev@gmail.com>
2018-10-31 19:15:55 -0300

    Continue CalendarBox

--- src/components/CalendarBox.js
+++ src/components/CalendarBox.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { View } from 'react-native'
 import { Surface, Text } from 'react-native-paper'
+import PropTypes from "prop-types";
 
 export default class CalendarBox extends PureComponent {
   constructor(props) {
@@ -10,10 +11,13 @@ export default class CalendarBox extends PureComponent {
   }
 
   render() {
+    const {
+        calendarText
+      } = this.props;
     return (
         <Surface style={styles.calendar}>
         <Text style={styles.calendarHeaderText}>
-          {'Retirada'.toUpperCase()}
+          {calendarText.toUpperCase()}
         </Text>
         <Text style={styles.calendarContentText}>
           10/09
@@ -22,3 +26,36 @@ export default class CalendarBox extends PureComponent {
     );
   }
 }
+
+CalendarBox.propTypes = {
+    
+}
+
+
+const styles = StyleSheet.create({
+    calendar: {
+        flex: 0.5,
+        borderRadius: 8,
+        elevation: 4,
+      },
+      calendarHeaderText: {
+        height: 22,
+        backgroundColor: Colors.calendarRed,
+        fontSize: 14,
+        textAlign: 'center',
+        fontWeight: '500',
+        color: Colors.lightGrayFont,
+        borderTopRightRadius: 8,
+        borderTopLeftRadius: 8,
+      },
+      calendarContentText: {
+        fontSize: 22,
+        marginTop: 14,
+        textAlign: 'center',
+        fontWeight: '500',
+        fontSize: 24,
+        color: Colors.titleDarkFont,
+        borderTopRightRadius: 8,
+        borderTopLeftRadius: 8,
+      },
+});
